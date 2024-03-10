7b10107cc20ef7e6016dadf267b4e6de08612d8c
Urbani Michele <michele.urbani094@gmail.com>
2024-03-10 20:39:31 +0100

    ADD: Correzione esercizio

--- JS/app.js
+++ JS/app.js
@@ -1,33 +1,47 @@
 
 // Verificare il numero di chilometri che vuole percorrere il passeggero
-const chilometri = 47;    //number
-console.log(chilometri)
+
+const kmViaggio = parseInt(prompt('Inserisci il numero di km del viaggio'));    // number
+console.log(kmViaggio, typeof kmViaggio)
 
 // Verificare l'età del passeggero
-const eta = 16;     //number
-console.log(eta)
+
+const eta = parseInt(prompt('Inserisci la tua età'));   // number
+console.log(eta, typeof eta)
+
+const prezzoPerKm = 0.21;   // number
 
 // Verificare il prezzo totale del viaggio
- 
-// SE il passeggero percorre 47km
-//  ALLORA il prezzo è definito dai km percorsi
-//  ALLORA scrivo in console il 'prezzo totale'
-const prezzoTot = 47 * 0.21;
-console.log(prezzoTot)
+// - calcolare il prezzo base sapendo che ogni km costa 0.21€
 
-// SE il passeggero è <18 anni
-//  ALLORA srivo in console 'sconto20%'
+const prezzoBase = parseFloat(kmViaggio * prezzoPerKm); // number
+console.log(prezzoBase, typeof prezzoBase)
 
-// ALTRIMENTI SE il passeggero è >65 anni
-//  ALLORA scrivo in console 'sconto40%'
+// - calcolare lo sconto
 
-// ALTRIMENTI il passeggero non avrà lo sconto
+let sconto // undefined  /*Posso applicare lo sconto = 0 e togliere l'else finale, il risultato non cambia //number */
+
+// -- SE età passeggero <18 = sconto 20%
 
 if (eta < 18) {
-    console.log('Applica sconto 20%')
+    sconto = prezzoBase * 0.2
 } else if (eta > 65) {
-    console.log('Applica sconto 40%')
+    // -- ALTRIMENTI SE età passeggero >65 = sconto40%
+    sconto = prezzoBase * 0.4
+} else {
+    // -- ALTRIMENTI sconto = 0%
+    sconto = 0
 }
 
+// - calcolare il prezzo finale = prezzo base - sconto
+// Stampare il prezzo con 2 cifre decimali
 
+const prezzo = prezzoBase - sconto; // numebr  /*Possiamo ottimizzare con prezzoBase * sconto aggiungendo sconto = 0 e togliento tutti i prezzoBase dall'if ed else*/
+console.log(prezzo.toFixed(2), typeof prezzo, prezzo)
+
+// Bonus: stampare biglietto nell'HTML con un tag 'p'
+
+const prezzoElemento = document.getElementById('prezzo'); 
+// console.log(prezzoElemento)
+prezzoElemento.innerHTML = 'Prezzo del biglietto: ' + prezzo.toFixed(2) + '&euro;'
 
